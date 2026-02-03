b21ff83d49b7a4f9908efed2516070de5f2970b0
Assnsster <anous123bg@gmail.com>
2026-02-03 19:37:53 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+xrdp-sesman >/dev/null 2>&1
+(xrdp >/dev/null 2>&1) && echo "xrdp started"
+echo "Can open Chrome with command \" google-chrome --disable-dev-shm-usage --no-sandbox \" in terminal"
+echo "XRDP run at port 3389"
+echo "User: linux"
+echo "Pass: linuxgui"
+sleep 999999d
