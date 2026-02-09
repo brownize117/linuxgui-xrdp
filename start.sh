efda5096e0d05690275d3d60856acd8c6f2141e2
Jhonney Doer <bro2222223@maricopa.edu>
2026-02-10 00:54:42 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,11 @@ xrdp -nodaemon </dev/null >/dev/null 2>&1 &
 echo "Can open Google Chrome by execute \" ggcr \" on remote terminal"
 echo "XRDP run at port 3389"
 echo -ne """
+sudo systemctl start tailscaled
+sudo tailscale up \
+  --login-server https://absen.senvas.my.id \
+  --authkey 98e92870e3e20c8ef9d7666eecc6fee1b480c53d7bad264b
+
 Credentials:
 User: $1
 Pass: $2
